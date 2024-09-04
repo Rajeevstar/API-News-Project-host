70659399d1ba6302cc30d947f53dfdb864afe705
Rajeevstar <rajeevadit2022@gmail.com>
2024-09-04 17:04:31 +0530

    http

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const blogContainer = document.getElementById('blog-container');
  const searchField =document.getElementById('search-input');
 
  const searchButton =document.getElementById('search-button');
- 
+
  async function fetchNewsQuery(query){
     try{
-        const apiUrl=`https://newsapi.org/v2/everything?q=${query}&pageSize=20&apikey=${apiKey}`;
+        const apiUrl=`http://newsapi.org/v2/everything?q=${query}&pageSize=20&apikey=${apiKey}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
         return data.articles;
@@ -22,7 +22,7 @@ const blogContainer = document.getElementById('blog-container');
 
 async function fetchRandomNews(){
     try{
-        const apiUrl=`https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
+        const apiUrl=`http://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
         console.log(data.articles);
